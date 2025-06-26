f325d75e8b5863b7f39c904bf0f7095f39e21f66
Joshna1412 <djoshna167@gmail.com>
2025-06-26 16:39:45 +0530

    first commit

--- src/component/LossCard/index.jsx
+++ src/component/LossCard/index.jsx
@@ -10,10 +10,7 @@ class LossCard extends Component {
         return (
             <div className='loss-card-container'>
                 <div>
-                    <h1>You Lose</h1>
-                    <h3>Score</h3>
-                    <h1 className='score-reveal'>{score}/12</h1>
-                    <button className='play-again' onClick={start}>Play Again</button>
+
                 </div>
                 <div>
                     <img className='loss-emoji' src="https://assets.ccbp.in/frontend/react-js/lose-game-img.png" alt="loss-emoji" />
